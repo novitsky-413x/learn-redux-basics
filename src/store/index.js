f64614be8d65f7426dab4858119e92c426230dcb
Alexander Novitsky <novitsky.413x@gmail.com>
2025-03-19 11:43:32 +0300

    fixed showCounter for increment/decrement actions in store

--- src/store/index.js
+++ src/store/index.js
@@ -9,11 +9,13 @@ const counterReducer = (state = initialState, action) => {
     if (action.type === 'INCREMENT') {
         return {
             counter: state.counter + 1,
+            showCounter: state.showCounter,
         };
     }
     if (action.type === 'DECREMENT') {
         return {
             counter: state.counter - 1,
+            showCounter: state.showCounter,
         };
     }
     if (action.type === 'INCREASE') {
